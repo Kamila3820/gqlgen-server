477ff2879d908f5b965d884db7f1a51f3258606d
Thanadol Saojarkaval <98137663+Kamila3820@users.noreply.github.com>
2025-01-15 20:59:01 +0700

    - initial httpserve with echo

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"gqlgen-server/graph"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/labstack/echo/v4"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	e := echo.New()
+
+	queryHandler := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	e.POST("/query", func(c echo.Context) error {
+		queryHandler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+
+}
